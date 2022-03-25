eb95cdc30b9e62612ae6ee3bcb97da60ea02f486
wcjameson <williamjameson90@gmail.com>
2022-03-25 13:53:01 -0700

    add Flavor Controller

---
+++ PierresTreats/Controllers/FlavorController.cs
@@ -0,0 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PierresTreats.Controllers
+{
+  public class FlavorController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public FlavorController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+      .Include(thisFlavor => thisFlavor.JoinEntities)
+      .ThenInclude(join => join.Treat)
+      .FirstOrDefault(thisFlavor => thisFlavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
+    }
+  }
+}
